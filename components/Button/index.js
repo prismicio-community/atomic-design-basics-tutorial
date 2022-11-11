c801e204b8870f2514a389a4db7fd4bacbbad5c3
gracemiller23 <gracemiller0323@gmail.com>
2022-11-11 14:25:29 -0600

    Removed code for tutorial steps and added TODO comments

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,9 +1,1 @@
-import { PrismicLink } from '@prismicio/react'
-
-export const Button = ({ buttonLink, buttonText }) => {
-  return (
-    <PrismicLink field={buttonLink} className='rounded bg-white px-5 py-3 font-medium text-slate-800'>
-      {buttonText || 'Learn More'}
-    </PrismicLink>
-  )
-}
+//TODO: Add your Button atom here.
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,11 @@
-import Head from "next/head";
-import { SliceZone } from "@prismicio/react";
-import * as prismicH from "@prismicio/helpers";
 
-import { createClient } from "../prismicio";
-import { components } from "../slices/";
-import { Layout } from "../components/Layout";
-
-const Index = ({ page, navigation, settings }) => {
+//TODO: Copy in the code for your Template, which brings together 
+// all of the elements of your atomic design, and connects them to dynamic content.
+const Index = ({ }) => {
   return (
-    <Layout navigation={navigation} settings={settings}>
-      <Head>
-        <title>{prismicH.asText(page.data.title)}</title>
-      </Head>
-      <SliceZone slices={page.data.slices} components={components} />
-    </Layout>
+<h2>Your atomic design - coming soon!</h2>
   );
 };
 
 export default Index;
 
-export async function getStaticProps({ locale, previewData }) {
-  const client = createClient({ previewData });
-
-  const page = await client.getByUID("page", "home", { lang: locale });
-  const navigation = await client.getSingle("navigation", { lang: locale });
-  const settings = await client.getSingle("settings", { lang: locale });
-
-  return {
-    props: {
-      page,
-      navigation,
-      settings,
-    },
-  };
-}
